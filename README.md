08364fd0820ad94a575ea1758eb42d01510b22a6
Eric Wu <wueric@stanford.edu>
2020-11-20 21:08:39 -0800

    print messages and documentation

---
+++ README.md
@@ -0,0 +1,38 @@
+## Alternative STA calculation methods
+
+### Method 1: Temporal resampling of the white noise stimulus movie
+
+Temporal resampling allows the user to set an arbitrary frequency F (120 Hz, 240 Hz, 102 Hz, anything really) and 
+bins both spikes and frames into bins of width 1/F, and calculates STAs with an effective frequency of F. 
+This frequency is measured relative to the 20 kHz sample rate of the array, so it is more robust than estimating
+the timing from the TTL channel (for the traditional Vision method of calculating STAs, where spikes are binned into
+the stimulus frames, the length of each bin varies a little bit based on the temporal jitter of the monitor refresh rate,
+whereas here the bin widths are constant).
+
+Temporal resampling also enables us to supersample the stimulus movie. For example, even if the stimulus refreshes at 120 Hz,
+we can calculate an STA at a higher frequency (i.e. 240 Hz) by resampling the stimulus at the higher frequency. In effect,
+this allows us to incorporate information about when within a frame a given cell spiked (the intuitive hypothesis here is that knowing
+whether a cell spiked at the beginning, in the middle, or at the end of a frame interval contains additional information about
+the timecourse).
+
+Note that even if we set the resampling frequency at some integer multiple of the monitor refresh rate, the resampled bin
+intervals will eventually go out-of-sync with the monitor refresh intervals, because of temporal jitter in the frame rate.
+
+Approximate runtime: about 4-8 minutes for 30 minute white noise stimulus. Stixel size doesn't appear to strongly affect
+the runtime.
+
+#### Synopsis
+
+```shell script
+python bin_frames_by_spike_times.py <analysis-path> <ds-name> <path-to-stimulus-xml> <path-to-save-output-pickle> [flags]
+```
+#### Options
+* **-r**, **--frame-rate**, resampling frequency, the frequency to calculate the STAs at. Default value 120.0 
+* **-n**, **--n-frames**, STA depth, default value 51
+* **-b**, **--batch**, number of cells per batch. Default 512. Larger runs faster, too large and GPU runs out of space
+
+#### Example
+
+```shell script
+CUDA_VISIBLE_DEVICES=3, python bin_frames_by_spike_times.py /Volumes/Lab/Users/ericwu/yass-reconstruction/2017-12-04-5/data001 data001 /Volumes/Analysis/stimuli/white-noise-xml/RGB-10-4-0.48-11111.xml /Volumes/Scratch/Users/wueric/debug/2017_12_04_5_data001_example -r 240 -n 51
+```
--- bin_frames_by_spike_times.py
+++ bin_frames_by_spike_times.py
@@ -1,26 +1,17 @@
 from lib.torch_sta import bin_frames_by_spike_times
 from lib.save_data import generate_save_dict
 
-import numpy as np
 import torch
 
 import visionloader as vl
-import visionwriter as vw
 from whitenoise import RandomNoiseFrameGenerator
 
 import pickle
-
 import argparse
-from typing import Dict, Tuple, Union, Sequence
-import os
 
 CELL_BATCH_SIZE = 512
 N_DISPLAY_FRAMES_PER_TTL = 100
 SAMPLE_FREQ = 20000
-
-
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -31,9 +22,8 @@ if __name__ == '__main__':
     parser.add_argument('xml_path', type=str, help='path to stimulus XML file')
     parser.add_argument('output', type=str, help='path to save location')
     parser.add_argument('-r', '--frame_rate', type=float, help='effective frame rate [Hz]', default=120.0)
-    parser.add_argument('-n', '--n_frames', type=int, help='number of frames', default=51)
+    parser.add_argument('-n', '--n_frames', type=int, help='STA depth', default=51)
     parser.add_argument('-b', '--batch', type=int, help='number of cells batch size', default=CELL_BATCH_SIZE)
-    parser.add_argument('-v', '--visionwriter', type=bool, default=False, help='save in Vision .sta format')
 
     args = parser.parse_args()
 
@@ -41,7 +31,6 @@ if __name__ == '__main__':
     torch.set_num_threads(8)
 
     print("Loading spike times...")
-
     dataset = vl.load_vision_data(args.ds_path, args.ds_name, include_neurons=True)
     all_cells = dataset.get_cell_ids()
     spike_times_dict = {cell_id: dataset.get_spike_times_for_cell(cell_id) for cell_id in all_cells}
@@ -61,35 +50,7 @@ if __name__ == '__main__':
                                          args.batch,
                                          device)
 
-    if args.visionwriter:
-
-        sta_container_by_cell_id = {}  # type: Dict[int, vl.STAContainer]
-        for cell_id, sta_matrix in sta_dict.items():
-            depth, width, height, n_channels = sta_matrix.shape
-            no_error = np.zeros_like(sta_matrix[..., 0])
-            sta_container = vl.STAContainer(framegen.stixel_width,
-                                            1.0 / args.frame_rate,
-                                            0,
-                                            sta_matrix[..., 0],
-                                            no_error,
-                                            sta_matrix[..., 1],
-                                            no_error,
-                                            sta_matrix[..., 2],
-                                            no_error)
-            sta_container_by_cell_id[cell_id] = sta_container
-
-        with vw.STAWriter(args.output,
-                          args.ds_name,
-                          framegen.field_width,
-                          framegen.field_height,
-                          args.n_frames,
-                          1.0 / args.frame_rate,
-                          0,
-                          framegen.stixel_width) as staw:
-
-            staw.write_sta_by_cell_id(sta_container_by_cell_id)
-
-    else:
-        with open(args.output, 'wb') as pfile:
-            save_dict = generate_save_dict(sta_dict, args.frame_rate)
-            pickle.dump(save_dict, pfile)
+    print("Writing STAs")
+    with open(args.output, 'wb') as pfile:
+        save_dict = generate_save_dict(sta_dict, args.frame_rate)
+        pickle.dump(save_dict, pfile)
